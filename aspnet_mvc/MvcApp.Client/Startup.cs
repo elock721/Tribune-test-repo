5c9b33b8667ef00a8b9ef294ccc84228408c4784
elliott lockwood <lockwood721@gmail.com>
2021-02-02 00:05:03 -0600

    test3

--- aspnet_mvc/MvcApp.Client/Startup.cs
+++ aspnet_mvc/MvcApp.Client/Startup.cs
@@ -50,7 +50,7 @@ namespace MvcApp.Client
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Landing}/{action=Index}/{id?}");
             });
         }
     }
