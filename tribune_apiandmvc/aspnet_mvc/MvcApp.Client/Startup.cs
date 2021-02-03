a56edf170ae200e03bfe41d5be055a4289143119
elliott lockwood <lockwood721@gmail.com>
2021-02-02 21:40:20 -0600

    2 image test

--- tribune_apiandmvc/aspnet_mvc/MvcApp.Client/Startup.cs
+++ tribune_apiandmvc/aspnet_mvc/MvcApp.Client/Startup.cs
@@ -8,6 +8,10 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Okta.AspNetCore;
 
 namespace MvcApp.Client
 {
@@ -23,8 +27,27 @@ namespace MvcApp.Client
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+          services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = OktaDefaults.MvcAuthenticationScheme;
+            })
+            .AddCookie()
+            .AddOktaMvc(new OktaMvcOptions
+            {
+                // Replace these values with your Okta configuration
+                OktaDomain = Configuration["oktaDomain"],
+                ClientId = Configuration["oktaClient"],
+                ClientSecret = Configuration["oktaSecret"]
+            });
             services.AddControllersWithViews();
+
         }
+
+
+
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -37,13 +60,13 @@ namespace MvcApp.Client
             {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+
             }
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
+            // app.UseHttpsRedirection();
+            // app.UseStaticFiles();
 
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
